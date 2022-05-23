3b555867a06df29108e23eb6cc80bc8499c2c854
Sophia C <95388964+Sophtron5000@user.noreply.github.com>
2022-05-22 22:32:05 -0700

    add friends list api & controller files, and add route to api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user');
+const friendsRoutes = require('./friends')
 // const thoughtRoutes = require('./thoughtRoutes');
 
 router.use('/user', userRoutes);
+router.use('/friends', friendsRoutes);
 // router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
